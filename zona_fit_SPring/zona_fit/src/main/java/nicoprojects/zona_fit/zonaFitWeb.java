3d1fba4141f4835ede34cd1f8d4a7e40345b9629
Hector Ramirez <hnramirez04@gmail.com>
2025-06-26 18:42:00 -0600

    Add web configuration to work wit JSF

---
+++ zona_fit_SPring/zona_fit/src/main/java/nicoprojects/zona_fit/zonaFitWeb.java
@@ -0,0 +1,12 @@
+package nicoprojects.zona_fit;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class zonaFitWeb {
+    public static void main(String[] args) {
+        SpringApplication.run(zonaFitWeb.class,args);
+    }
+}
